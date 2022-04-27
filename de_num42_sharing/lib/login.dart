6e0f8509ebf2967aef68640576672eb07950bd3e
emuguy1 <emanuel.erben@gmail.com>
2022-04-27 16:25:36 +0200

    Added learnings from tutorial and added second screen and added margin

--- de_num42_sharing/lib/login.dart
+++ de_num42_sharing/lib/login.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 
 class LoginRoute extends StatelessWidget {
-  const LoginRoute({Key key}) : super(key: key);
+  const LoginRoute({Key? key}) : super(key: key);
 
 
   @override
