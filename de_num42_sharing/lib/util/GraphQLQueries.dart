246a8471be0cce81c41cd52393d7613e3371395c
emuguy1 <emanuel.erben@gmail.com>
2022-06-17 22:53:55 +0200

    Moved Queries to a extra file

---
+++ de_num42_sharing/lib/util/GraphQLQueries.dart
@@ -0,0 +1,109 @@
+var getItemsQuery = """query GetItems(\$id: ID!, \$cursor: String) {
+                          node (id: \$id) {
+                              id
+                              ...on Person{
+                                  id
+                                  items(first: 10, after:\$cursor){
+                                      edges{
+                                          node {
+                                              id
+                                              ... on Item{
+                                                  id
+                                                  name
+                                                  description
+                                              }
+                                          }
+                                      }
+                                      pageInfo {
+                                          hasNextPage
+                                          endCursor
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                      """;
+
+var getConversationsQuery =
+    """query GetConversations(\$id: ID!, \$cursor: String) {
+        node (id: \$id) {
+            id
+            ...on Person{
+                firstName
+                lastName
+                conversations(first: 10, after: \$cursor){
+                    edges{
+                        node {
+                            ... on Conversation{
+                                id
+                                lastMessage
+                                lastMessageAt
+                                participants {
+                                    id
+                                    firstName
+                                    lastName
+                                }
+                            }
+                        }
+                    }
+                    pageInfo {
+                        hasNextPage
+                        endCursor
+                    }
+                }
+            }
+        }
+    }
+    """;
+
+var loginMutation =
+"""mutation login(\$email: String!, \$password: String!) {
+                        login(email: \$email, password: \$password)
+                      }
+                      """;
+
+var meQuery =
+  """query me {
+    me {
+      id
+      firstName
+      lastName
+    }
+  }""";
+
+var messagesFromIdQuery =
+    """query GetMessages(\$id: ID!, \$cursor: String) {
+    node (id: \$id) {
+        id
+        ...on Conversation{
+            participants {
+                firstName
+                lastName
+                id
+            }
+            messages(first: 10, after: \$cursor){
+                edges {
+                    node{
+                        id
+                        ...on Message{
+                            message
+                            timestamp
+                            readAt
+                            recipient {
+                                id
+
+                            }
+                            sender {
+                                id
+                            }
+                        }
+                    }
+                }
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+            }
+        }
+    }
+}""";
